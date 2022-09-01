cd8d3e33b9d08e6fdd997ff56fc798862338bea8
JunTingLin <10355098@st.thsh.tp.edu.tw>
2022-09-02 04:10:43 +0800

    10-Dynamic proxy, static proxy

--- 10-spring-proxy/src/main/java/com/atguigu/spring/proxy/ProxyFactory.java
+++ 10-spring-proxy/src/main/java/com/atguigu/spring/proxy/ProxyFactory.java
@@ -19,7 +19,7 @@ public class ProxyFactory {
         this.target = target;
     }
 
-    public Object getProxy(){
+    public Object getProxy() {
         /**
          * ClassLoader loader：指定加载动态生成的代理类的类加载器
          * Class[] interfaces：获取目标对象实现的所有接口的class对象的数组
@@ -27,20 +27,20 @@ public class ProxyFactory {
          */
         ClassLoader classLoader = this.getClass().getClassLoader();
         Class<?>[] interfaces = target.getClass().getInterfaces();
-        InvocationHandler h = new InvocationHandler() {
+        InvocationHandler h = new InvocationHandler() { //匿名函數
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 Object result = null;
                 try {
-                    System.out.println("日志，方法："+method.getName()+"，参数："+ Arrays.toString(args));
+                    System.out.println("日志，方法：" + method.getName() + "，参数：" + Arrays.toString(args));
                     //proxy表示代理对象，method表示要执行的方法，args表示要执行的方法到的参数列表
                     result = method.invoke(target, args);
-                    System.out.println("日志，方法："+method.getName()+"，结果："+ result);
+                    System.out.println("日志，方法：" + method.getName() + "，结果：" + result);
                 } catch (Exception e) {
                     e.printStackTrace();
-                    System.out.println("日志，方法："+method.getName()+"，异常："+ e);
+                    System.out.println("日志，方法：" + method.getName() + "，异常：" + e);
                 } finally {
-                    System.out.println("日志，方法："+method.getName()+"，方法执行完毕");
+                    System.out.println("日志，方法：" + method.getName() + "，方法执行完毕");
                 }
                 return result;
             }
